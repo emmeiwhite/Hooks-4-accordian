392111fe183b8344a8d624cde7e746da77e5dece
Imran Rafiq Rather <emmeiwhite@gmail.com>
2020-11-23 07:02:14 +0530

    Using Rect-Icons

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,8 +8,12 @@ const Question = ({ title, info, id }) => {
     <article key={id} className="question-answer-card">
       <div className="question">
         <div>{title}</div>
-        <div onClick={() => setIsClicked(!isClicked)}>
-          {isClicked ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        <div onClick={() => setIsClicked(!isClicked)} className="icon-box">
+          {isClicked ? (
+            <AiOutlineMinus className="icon" />
+          ) : (
+            <AiOutlinePlus className="icon" />
+          )}
         </div>
       </div>
       {isClicked && <div>{info}</div>}
